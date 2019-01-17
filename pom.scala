6d73989d070b377790355793160ac7006ddb6d36
Andreas Gies <andreas@wayofquality.de>
2019-01-17 10:12:19 +0100

    Updating version

--- pom.scala
+++ pom.scala
@@ -4,8 +4,8 @@ import scala.collection.immutable.Seq
 
 implicit val scalaVersion = ScalaVersion("2.12.8")
 
-val pluginVersion = "3.0-M4-SNAPSHOT"
-val blendedVersion = "3.0-M4-SNAPSHOT"
+val pluginVersion = "3.1-SNAPSHOT"
+val blendedVersion = "3.1-SNAPSHOT"
 
 object Deps {
   val mavenVersion = "3.0.5"
