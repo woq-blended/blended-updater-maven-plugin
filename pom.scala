c0e58ac86509d83cf87d79c96467733df3c6c8e9
Andreas Gies <andreas@wayofquality.de>
2019-03-05 12:09:25 +0100

    Releasing 3.0-M5

--- pom.scala
+++ pom.scala
@@ -4,8 +4,8 @@ import scala.collection.immutable.Seq
 
 implicit val scalaVersion = ScalaVersion("2.12.8")
 
-val pluginVersion = "3.0-M4"
-val blendedVersion = "3.0-M4"
+val pluginVersion = "3.0-M5"
+val blendedVersion = "3.0-M5"
 
 object Deps {
   val mavenVersion = "3.0.5"
