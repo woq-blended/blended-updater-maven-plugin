5966a0b051f726e1a41bf74a82611d785f18d81f
Andreas Gies <andreas@wayofquality.de>
2019-04-30 14:56:55 +0200

    After releasing 3.0-M9

--- pom.scala
+++ pom.scala
@@ -4,8 +4,8 @@ import scala.collection.immutable.Seq
 
 implicit val scalaVersion = ScalaVersion("2.12.8")
 
-val pluginVersion = "3.0-M9"
-val blendedVersion = "3.0-M9"
+val pluginVersion = "3.0-M10-SNAPSHOT"
+val blendedVersion = "3.0-M10-SNAPSHOT"
 
 object Deps {
   val mavenVersion = "3.0.5"
