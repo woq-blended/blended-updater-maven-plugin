0c183095db418b9a798ae14cd33bc987d9a7ae78
Andreas Gies <andreas@wayofquality.de>
2019-07-01 21:38:38 +0200

    Releasing 3.0-M17

--- pom.scala
+++ pom.scala
@@ -4,8 +4,8 @@ import scala.collection.immutable.Seq
 
 implicit val scalaVersion = ScalaVersion("2.12.8")
 
-val pluginVersion = "3.0-M17-SNAPSHOT"
-val blendedVersion = "3.0-M17-SNAPSHOT"
+val pluginVersion = "3.0-M17"
+val blendedVersion = "3.0-M17"
 
 object Deps {
   val mavenVersion = "3.0.5"
