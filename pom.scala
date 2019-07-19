95775efe2307f9077f10a09d31643aec5aab2682
Andreas Gies <andreas@wayofquality.de>
2019-07-19 12:42:40 +0200

    Releasing 3.0-M19

--- pom.scala
+++ pom.scala
@@ -4,8 +4,8 @@ import scala.collection.immutable.Seq
 
 implicit val scalaVersion = ScalaVersion("2.12.8")
 
-val pluginVersion = "3.0-M19-SNAPSHOT"
-val blendedVersion = "3.0-M19-SNAPSHOT"
+val pluginVersion = "3.0-M19"
+val blendedVersion = "3.0-M19"
 
 object Deps {
   val mavenVersion = "3.0.5"
