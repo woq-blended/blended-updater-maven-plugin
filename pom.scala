0161ed44c5de0c1e5ae197a91fa54aa9c58aeec7
Andreas Gies <andreas@wayofquality.de>
2019-07-28 17:01:08 +0200

    Releasing 3.0-M22

--- pom.scala
+++ pom.scala
@@ -4,8 +4,8 @@ import scala.collection.immutable.Seq
 
 implicit val scalaVersion = ScalaVersion("2.12.8")
 
-val pluginVersion = "3.0-M21"
-val blendedVersion = "3.0-M21"
+val pluginVersion = "3.0-M22"
+val blendedVersion = "3.0-M22"
 
 object Deps {
   val mavenVersion = "3.0.5"
