bc8f2812c2a621751b192effb322cf599e019df7
Andreas Gies <andreas@wayofquality.de>
2019-08-22 19:54:11 +0200

    Bumping versions

--- pom.scala
+++ pom.scala
@@ -4,8 +4,8 @@ import scala.collection.immutable.Seq
 
 implicit val scalaVersion = ScalaVersion("2.12.8")
 
-val pluginVersion = "3.0.2"
-val blendedVersion = "3.0.2"
+val pluginVersion = "3.0.3"
+val blendedVersion = "3.0.3"
 
 object Deps {
   val mavenVersion = "3.0.5"
