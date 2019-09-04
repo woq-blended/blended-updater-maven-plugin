8e432cda1b48d0733c30c7a9c92972e3b39da82f
Andreas Gies <andreas@wayofquality.de>
2019-09-04 22:16:00 +0200

    Releasing 3.0.4

--- pom.scala
+++ pom.scala
@@ -4,8 +4,8 @@ import scala.collection.immutable.Seq
 
 implicit val scalaVersion = ScalaVersion("2.12.8")
 
-val pluginVersion = "3.0.4-SNAPSHOT"
-val blendedVersion = "3.0.4-SNAPSHOT"
+val pluginVersion = "3.0.4"
+val blendedVersion = "3.0.4"
 
 object Deps {
   val mavenVersion = "3.0.5"
