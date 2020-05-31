fb0c9d4e9a085b2acbe16eea8444795c2f7bd271
Andreas Gies <andreas@wayofquality.de>
2020-05-31 20:10:09 +0200

    Releasing 3.0.14

--- pom.scala
+++ pom.scala
@@ -4,8 +4,8 @@ import scala.collection.immutable.Seq
 
 implicit val scalaVersion = ScalaVersion("2.12.8")
 
-val pluginVersion = "3.0.13"
-val blendedVersion = "3.0.13"
+val pluginVersion = "3.0.14"
+val blendedVersion = "3.0.14"
 
 object Deps {
   val mavenVersion = "3.0.5"
